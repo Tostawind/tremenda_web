272c6cdf5db9959178a7eeabb6d8f41d615b66e5
Abel <abel.dieguez@wakasolutions.com>
2023-09-26 13:18:30 +0200

    add: cover

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#D9D9D9',
+				secondary: '#000000',
+				accent: '#F50187',
+				'accent-darker' : '#dd0478' 
+			},
+			fontFamily: {
+				'montserrat': ['Montserrat', 'sans-serif']
+			},
+		},
 	},
 	plugins: [],
 }
